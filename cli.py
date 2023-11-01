b7355a46ba6efa6aea64be3ee51949321aa884af
Jassie2023 <148395308+Jassie2023@users.noreply.github.com>
2023-10-31 20:05:17 -0700

    Update cli.py
    NEWNEWNEW

--- cli.py
+++ cli.py
@@ -1,68 +1,64 @@
-# This file contains the Command Line Interface (CLI) for
-# the Tic-Tac-Toe game. This is where input and output happens.
-# For core game logic, see logic.py.
-
-from logic import make_empty_board
-
-# Reminder to check all the tests
+def make_empty_board():
+    return [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
 
 def print_board(board):
     for row in board:
-        print(" | ".join(row))
+        print(" | ".join(str(cell) if cell is not None else " " for cell in row))
         print("-" * 9)
 
-def check_win(board, player):
+def get_winner(board):
+    """Determines the winner of the given board.
+    Returns 'X', 'O', or None."""
     for i in range(3):
         # Check horizontal wins
-        if board[i][0] == board[i][1] == board[i][2] == player:
-            return True
+        if board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
 
         # Check vertical wins
-        if board[0][i] == board[1][i] == board[2][i] == player:
-            return True
+        if board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
 
     # Check diagonal wins
-    if board[0][0] == board[1][1] == board[2][2] == player:
-        return True
-    if board[0][2] == board[1][1] == board[2][0] == player:
-        return True
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
 
-    return False
+    return None
 
-def is_board_full(board):
-    return all(cell != " " for row in board for cell in row)
+def other_player(player):
+    """Given the character for a player, returns the other player."""
+    return 'X' if player == 'O' else 'O'
 
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
-    while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        # TODO: Input a move from the player.
-        # TODO: Update the board.
-        # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
-        current_player = "X"
+    current_player = 'X'
 
-    while True:
+    while winner is None:
         print_board(board)
         print(f"Player {current_player}'s turn")
         row = int(input("Enter row (0, 1, or 2): "))
         col = int(input("Enter column (0, 1, or 2): "))
 
-        if board[row][col] == " ":
+        if board[row][col] is None:
             board[row][col] = current_player
+            winner = get_winner(board)
 
-            if check_win(board, current_player):
+            if winner:
                 print_board(board)
-                print(f"Player {current_player} wins!")
+                print(f"Player {winner} wins!")
                 break
 
-            if is_board_full(board):
+            if all(cell is not None for row in board for cell in row):
                 print_board(board)
                 print("It's a tie!")
                 break
 
-            current_player = "O" if current_player == "X" else "X"
+            current_player = other_player(current_player)
         else:
             print("That position is already taken. Try again.")
