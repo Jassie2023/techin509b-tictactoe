2128c028282d6d87a6218c75fb3a0d1c323429be
Jassie2023 <148395308+Jassie2023@users.noreply.github.com>
2023-11-21 17:41:38 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,7 +1,6 @@
 # This file is where game logic lives. No input
 # or output happens here. The logic in this file
 # should be unit-testable.
-
 def make_empty_board():
     return [
         [None, None, None],
@@ -11,66 +10,73 @@ def make_empty_board():
 
 def print_board(board):
     for row in board:
-        print(" | ".join(str(cell) if cell is not None else " " for cell in row))
-        print("-" * 9)
-
-def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    for i in range(3):
-        # Check horizontal wins
-        if board[i][0] == board[i][1] == board[i][2]:
-            return board[i][0]
-
-        # Check vertical wins
-        if board[0][i] == board[1][i] == board[2][i]:
-            return board[0][i]
-
-    # Check diagonal wins
-    if board[0][0] == board[1][1] == board[2][2]:
-        return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0]:
-        return board[0][2]
-
-    return None
+        print(" ".join([str(cell) if cell is not None else ' ' for cell in row]))
 
 def move_step(board, row, col, player):
     if board[row][col] is None:
         board[row][col] = player
         return True
-    return False
+    else:
+        return False
+
+def get_winner(board):
+    # Implement your logic to check for a winner
+    pass
 
 def is_full(board):
-    return all(cell is not None for row in board for cell in row)
+    for row in board:
+        if None in row:
+            return False
+    return True
 
-if __name__ == '__main__':
-    board = make_empty_board()
-    current_player = 'X'
+class TicTacToeGame:
+    def __init__(self, player1, player2):
+        self.board = make_empty_board()
+        self.current_player = 'X'
+        self.player1 = player1
+        self.player2 = player2
 
-    while True:
-        print_board(board)
-        print(f"Player {current_player}'s turn")
+    def play_game(self):
+        while True:
+            print_board(self.board)
+            print(f"Player {self.current_player}'s turn")
 
-        try:
-            row = int(input("Enter row (0, 1, or 2): "))
-            col = int(input("Enter column (0, 1, or 2): "))
+            try:
+                if self.current_player == 'X':
+                    row, col = self.player1.make_move(self.board)
+                else:
+                    row, col = self.player2.make_move(self.board)
 
-            if row not in (0, 1, 2) or col not in (0, 1, 2):
-                raise ValueError("Invalid input. Row and column must be 0, 1, or 2.")
+                if row not in (0, 1, 2) or col not in (0, 1, 2):
+                    raise ValueError("Invalid input. Row and column must be 0, 1, or 2.")
 
-            if move_step(board, row, col, current_player):
-                winner = get_winner(board)
-                if winner:
-                    print_board(board)
-                    print(f"Player {winner} wins!")
-                    break
-                elif is_full(board):
-                    print_board(board)
-                    print("It's a draw!")
-                    break
-                current_player = 'X' if current_player == 'O' else 'O'
-            else:
-                print("Position is taken, you should try it again.")
-        except ValueError as e:
-            print(str(e))
-    
+                if move_step(self.board, row, col, self.current_player):
+                    winner = get_winner(self.board)
+                    if winner:
+                        print_board(self.board)
+                        print(f"Player {winner} wins!")
+                        break
+                    elif is_full(self.board):
+                        print_board(self.board)
+                        print("It's a draw!")
+                        break
+                    self.current_player = 'X' if self.current_player == 'O' else 'O'
+                else:
+                    print("Position is taken, you should try it again.")
+            except ValueError as e:
+                print(str(e))
+
+class HumanPlayer:
+    def make_move(self, board):
+        while True:
+            try:
+                row = int(input("Enter row (0, 1, or 2): "))
+                col = int(input("Enter column (0, 1, or 2): "))
+                return row, col
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+class BotPlayer:
+    def make_move(self, board):
+        import random
+        return random.randint(0, 2), random.randint(0, 2)
