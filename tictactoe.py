6a2c5f49ff0a4c286e428d58b3278a52151bf058
Jassie2023 <148395308+Jassie2023@users.noreply.github.com>
2023-11-28 18:35:56 -0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,44 +1,50 @@
 import csv
 import os
+import datetime  # For recording the timestamp
 
 # Define the path for the log file
 LOG_FILE_PATH = 'logs/game_log.csv'
 
 def record_winner(winner):
     with open(LOG_FILE_PATH, 'a', newline='') as csvfile:
-        fieldnames = ['Winner']
+        fieldnames = ['Winner', 'Timestamp']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         if os.path.getsize(LOG_FILE_PATH) == 0:
             writer.writeheader()
 
-        writer.writerow({'Winner': winner})
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        writer.writerow({'Winner': winner, 'Timestamp': timestamp})
 
-def record_game_statistics(player1, player2, winner, moves):
+def record_game_data(player1, player2, winner, total_moves):
     with open(LOG_FILE_PATH, 'a', newline='') as csvfile:
-        fieldnames = ['Player1', 'Player2', 'Winner', 'TotalMoves']
+        fieldnames = ['Player1', 'Player2', 'Winner', 'TotalMoves', 'Timestamp']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         if os.path.getsize(LOG_FILE_PATH) == 0:
             writer.writeheader()
 
-        writer.writerow({'Player1': player1, 'Player2': player2, 'Winner': winner, 'TotalMoves': moves})
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        writer.writerow({'Player1': player1, 'Player2': player2, 'Winner': winner, 'TotalMoves': total_moves, 'Timestamp': timestamp})
 
-# Update your TicTacToeGame class to include player names
+# Your existing game code remains unchanged
+
+# ...
+
+# In your TicTacToeGame class, where the game ends, call the record_winner and record_game_data functions:
+
 class TicTacToeGame:
-    def __init__(self, player1, player2, player1_name="Player 1", player2_name="Player 2"):
+    def __init__(self, player1, player2):
         self.board = make_empty_board()
         self.current_player = 'X'
         self.player1 = player1
         self.player2 = player2
-        self.player1_name = player1_name
-        self.player2_name = player2_name
 
     def play_game(self):
         total_moves = 0
         while True:
             print_board(self.board)
-            print(f"{self.player1_name}: X, {self.player2_name}: O")
+            print(f"Player {self.current_player}'s turn")
 
             try:
                 if self.current_player == 'X':
@@ -55,14 +61,16 @@ class TicTacToeGame:
 
                     if winner:
                         print_board(self.board)
-                        print(f"{self.player1_name if winner == 'X' else self.player2_name} wins!")
-                        record_winner(self.player1_name if winner == 'X' else self.player2_name)
-                        record_game_statistics(self.player1_name, self.player2_name, winner, total_moves)
+                        print(f"Player {winner} wins!")
+                        record_winner(winner)
+                        record_game_data("HumanPlayer" if self.current_player == 'X' else "BotPlayer",
+                                         "BotPlayer" if self.current_player == 'X' else "HumanPlayer",
+                                         winner, total_moves)
                         break
                     elif is_full(self.board):
                         print_board(self.board)
                         print("It's a draw!")
-                        record_game_statistics(self.player1_name, self.player2_name, 'Draw', total_moves)
+                        record_game_data("HumanPlayer", "BotPlayer", 'Draw', total_moves)
                         break
 
                     self.current_player = 'X' if self.current_player == 'O' else 'O'
@@ -70,5 +78,3 @@ class TicTacToeGame:
                     print("Position is taken, try again.")
             except ValueError as e:
                 print(str(e))
-
-# The rest of your code remains the same
